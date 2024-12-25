c0f040646207fa91d12562eff9eb65f6ea575710
UlasAtas <uatash@hotmail.com>
2024-12-25 17:06:22 +0300

    Controller ve Viewslar

---
+++ WebProje/WebProje/Controllers/KayitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProje.Controllers
+{
+	public class KayitController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
