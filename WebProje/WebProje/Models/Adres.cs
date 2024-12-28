8ebef7478b95bb1502560452fea6a6c3a18bdef9
kontevs <firdevskaya.43@gmail.com>
2024-12-28 18:45:14 +0300

    Models'e Adres.cs ekleme

--- WebProje/WebProje/Models/Adres.cs
+++ WebProje/WebProje/Models/Adres.cs
@@ -1,6 +1,15 @@
-﻿namespace WebProje.Models
+﻿using WebProje.Models;
+
+namespace WebProje.Models
 {
     public class Adres
     {
+        public int AdresId { get; set; }
+        public int KullaniciId { get; set; }
+        public string AdresSatiri { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string PostaKodu { get; set; }
+        public virtual Kullanici Kullanici { get; set; }
     }
 }
