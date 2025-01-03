5a26266993a7ab717cbe3a9ec544f1083161e286
kontevs <firdevskaya.43@gmail.com>
2025-01-03 17:52:39 +0300

    Ürünler sayfası

--- WebProje/WebProje/Controllers/UrunController.cs
+++ WebProje/WebProje/Controllers/UrunController.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProje.Models;
 
 namespace WebProje.Controllers
 {
 	public class UrunController : Controller
 	{
-		public IActionResult UrunDetay()
+        private readonly MyAppContext _context;
+
+        public UrunController(MyAppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult UrunDetay()
 		{
 			return View();
 		}
 
 		public IActionResult UrunListe()
-		{
+        {
+            
 
-			return View();
-		}
+            var urunler = _context.Urun;
+            //.Where(u => u.AktifMi)
+            //.OrderByDescending(u => u.OlusturmaTarihi).ToList();
 
-	}
+            return View(urunler);
+        }
+        
+        
+
+    }
 }
